1ef73a9e4e9ca7dcbf120ff43fe1c652c2d49960
Keeeweee <epsilonmajorquezero@gmail.com>
2018-08-10 12:42:39 +0200

    Add create product and Open Iconic

--- accounts/views.py
+++ accounts/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 
-# Create your views here.
 
+# Create your views here.
 def signup(request):
     if request.method == 'POST':
         if request.POST['password'] == request.POST['confirmation_password']:
             try:
-                user = User.objects.get(username=request.POST['username'])
+                User.objects.get(username=request.POST['username'])
                 return render(request, 'accounts/signup.html', {'error': 'Username has already been taken.'})
             except User.DoesNotExist:
-                User.objects.create_user(request.POST['username'], password=request.POST['password'])
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password'])
                 auth.login(request, user)
                 return redirect('home')
         else:
--- products/models.py
+++ products/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class Product(models.Model):
+    title = models.TextField()
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    url = models.TextField()
+    icon = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/')
+    votes_total = models.IntegerField(default=1)
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body.split('. ')[0] + '.'
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%e %b %Y')
